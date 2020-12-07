bf3f63723545245b4acfb119d89e9cf3cf31c3e0
Ezequiel Campos Pereira <zecaobh@gmail.com>
2020-12-07 13:13:10 -0300

    Create Param.cs

---
+++ ExportadorArquivosDSS/MainClasses/Param.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2.Principais
+{
+    struct Param
+    {
+        public string _codBase;
+        public string _pathAlim;
+        public string _conjAlim;
+        public string _trEM; //trecho energy meter
+    }
+}
