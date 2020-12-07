d5d1d66b2a0ba993c78f6a6b2c1f23600af9a958
Ezequiel Campos Pereira <zecaobh@gmail.com>
2020-12-07 13:12:07 -0300

    Create ArqManip.cs

---
+++ ExportadorArquivosDSS/AuxClasses/ArqManip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutorOpenDSS
+{
+    class ArqManip
+    {
+        //Verifica se o arquivo existe antes de deletá-lo
+        public static void SafeDelete(string arquivo)
+        {
+            if (File.Exists(arquivo))
+            {
+                File.Delete(arquivo);
+            }
+        } 
+
+        //Grava CONTEUDO em arquivo FID 
+        public static void GravaEmArquivo(string conteudo, string fid )
+        {
+            try
+            {
+                File.AppendAllText(fid, conteudo);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
