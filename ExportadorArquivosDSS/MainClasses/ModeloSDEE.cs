02dd95075239bc16142d99bcd28b0d91e7c31a86
Ezequiel Campos Pereira <zecaobh@gmail.com>
2020-12-07 13:13:06 -0300

    Create ModeloSDEE.cs

---
+++ ExportadorArquivosDSS/MainClasses/ModeloSDEE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2.Principais
+{
+    // opcoes de modelos SDEE 
+    class ModeloSDEE
+    {
+        public bool _usarCondutoresSeqZero = false;
+        public bool _utilizarCurvaDeCargaClienteMTIndividual = true;
+        public bool _incluirCapacitoresMT = false;
+        public bool _reatanciaTrafos = false;
+        public string _modeloCarga = "ANEEL"; // Modelos disponiveis: "ANEEL" "PCONST"
+        //public bool _incluirReatanciaDispersaoTrafos = true;
+
+        public ModeloSDEE(bool usarCondutoresSeqZero, bool utilizarCurvaDeCargaClienteMTIndividual, bool incluirCapacitoresMT, string modeloCarga, bool reatanciaTrafos)
+        {
+            _usarCondutoresSeqZero = usarCondutoresSeqZero;
+            _utilizarCurvaDeCargaClienteMTIndividual = utilizarCurvaDeCargaClienteMTIndividual;
+            _incluirCapacitoresMT = incluirCapacitoresMT;
+            _modeloCarga = modeloCarga;
+            _reatanciaTrafos = reatanciaTrafos;
+        }
+    }
+}
