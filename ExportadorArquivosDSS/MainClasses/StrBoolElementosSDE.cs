e3c461b4352660cf0b59b21162bd01a18d73ed98
Ezequiel Campos Pereira <zecaobh@gmail.com>
2020-12-30 00:34:07 -0300

    Update StrBoolElementosSDE.cs

--- ExportadorArquivosDSS/MainClasses/StrBoolElementosSDE.cs
+++ ExportadorArquivosDSS/MainClasses/StrBoolElementosSDE.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2.Principais
+namespace ExportadorGeoPerdasDSS
 {
     struct StrBoolElementosSDE
     {
