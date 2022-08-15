b35cc629722c031ac622df08b45524e05752244c
Ezequiel Campos Pereira <zecaobh@gmail.com>
2022-08-15 20:03:29 -0300

    Create ArqManip.cs

---
+++ AuxClasses/ArqManip.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace ExportadorGeoPerdasDSS
+{
+    class ArqManip
+    {
+        //Verifica se o arquivo existe antes de deletá-lo
+        public static void SafeDelete(string arquivo)
+        {
+            if (File.Exists(arquivo))
+            {
+                File.Delete(arquivo);
+            }
+        } 
+
+        //Grava CONTEUDO em arquivo FID 
+        public static void GravaEmArquivo(string conteudo, string fid )
+        {
+            try
+            {
+                File.AppendAllText(fid, conteudo);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
